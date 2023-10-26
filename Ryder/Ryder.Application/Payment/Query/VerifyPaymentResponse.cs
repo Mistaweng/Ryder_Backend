30b2dc39165f63806721e6b0eb2ca4b281619279
CalebTek <walexjackson@gmail.com>
2023-10-26 12:21:04 +0100

    Updated Order Payment and Verification Logic
    Takes OrderId and returns OrderId

--- Ryder/Ryder.Application/Payment/Query/VerifyPaymentResponse.cs
+++ Ryder/Ryder.Application/Payment/Query/VerifyPaymentResponse.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Ryder.Application.Payment.Query
 {
     public class VerifyPaymentResponse
     {
         public bool IsPaymentValid { get; set; }
         public string Message { get; set; }
+        public Guid OrderId { get; set; }
     }
 }
